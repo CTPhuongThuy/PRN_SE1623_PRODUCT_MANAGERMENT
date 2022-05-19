862f5973ed26c87dc1be51544cf0b531ae70ea05
tinh-nhi <nhatcao796569@gmail.com>
2022-05-19 13:49:52 +0700

    add to string

---
+++ PRN_SE1625_CLone/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿namespace Prn.Se1623;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public string Desc { get; set; }
+    public int CategorId { get; set; }
+    public double UnitPrice { get; set; }
+
+
+    public Product()
+    {
+
+    }
+
+    public Product(int id, string productName, string desc, 
+        int categorId, double unitPrice)
+    {
+        Id = id;
+        ProductName = productName;
+        Desc = desc;
+        CategorId = categorId;
+        UnitPrice = unitPrice;
+    }
+
+    public override string? ToString() => $"[Id = {Id}, Product name ={ProductName}, desc ={Desc}, " +
+                                             $"categoryId ={CategorId}, unit price ={UnitPrice}";
+
+
+
+}
